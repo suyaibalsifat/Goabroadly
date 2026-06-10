7c8923efc306ca5201053857bd3dc7bb7935aa5f
SUYAIBALSIFAT <suyaibalsifat@gmail.com>
2026-06-10 23:44:46 +0600

    feat: add all-offers endpoint for global marketplace data

--- routes/agencyRoutes.js
+++ routes/agencyRoutes.js
@@ -15,3 +15,4 @@ router.get("/agencies/:slug", agencyController.getAgencyProfileBySlug);
 // 🛠️ MAKE SURE THIS EXACT LINE IS AT THE VERY BOTTOM OF THE FILE:
 module.exports = router;
 // Export confirmation signature added
+// Exposed shared endpoint for public feed
